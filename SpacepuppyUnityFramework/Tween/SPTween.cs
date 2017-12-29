4892ddd1463d44ea6434c78706b6468416571afb
DYLAN-PC\Dylan <lordofduct@lordofduct.com>
2017-12-28 20:53:46 -0500

    Starting SPTween

---
+++ SpacepuppyUnityFramework/Tween/SPTween.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+
+using com.spacepuppy.Collections;
+
+namespace com.spacepuppy.Tween
+{
+
+    public class SPTween : ServiceComponent<SPTween>, IService
+    {
+
+    }
+
+}

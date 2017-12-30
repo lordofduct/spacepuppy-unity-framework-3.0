047d08b21785c02cfef17388fa9a468e6265087f
DYLAN-PC\Dylan <lordofduct@lordofduct.com>
2017-12-29 23:12:44 -0500

    Started SPSensors

---
+++ SPSensors/Sensors/IAspect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace com.spacepuppy.Sensors
+{
+
+    /// <summary>
+    /// Represents an object that can be recognized by a Sensor.
+    /// </summary>
+    public interface IAspect : IGameObjectSource
+    {
+
+        bool IsActive { get; }
+
+        float Precedence { get; }
+
+    }
+
+}

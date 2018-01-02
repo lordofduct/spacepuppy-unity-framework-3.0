9d72b91347eee4a97d6df5c63861c44219ea4411
DYLAN-PC\Dylan <lordofduct@lordofduct.com>
2018-01-02 01:47:43 -0500

    Added SPProject classes

---
+++ SPProject/Project/ITextSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace com.spacepuppy.Project
+{
+
+    public interface ITextSource : IEnumerable<string>
+    {
+
+        string text { get; }
+        string this[int index] { get; }
+        int Count { get; }
+
+    }
+
+}

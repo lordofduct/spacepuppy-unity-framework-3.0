e1917335a4e5f3798505e671d9cd0386ae94efa7
DYLAN-PC\Dylan <lordofduct@lordofduct.com>
2018-04-17 23:36:02 -0400

    Added ITokenizable

---
+++ SpacepuppyUnityFramework/ServiceProxy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace com.spacepuppy
+{
+
+    /// <summary>
+    /// Inherit from this class, and add a 'CreateAssetMenuAttribute' to create a asset that can be used as a proxy to a Service.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ServiceProxy<T> : ScriptableObject, IProxy where T : class, IService
+    {
+
+        public object GetTarget()
+        {
+            return Services.Get<T>();
+        }
+
+        public object GetTarget(object arg)
+        {
+            return Services.Get<T>();
+        }
+
+        public System.Type GetTargetType()
+        {
+            return typeof(T);
+        }
+
+    }
+
+}
